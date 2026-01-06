91efc2063c2e52c4fe707e8c999e4af041b0365c
Algos <2341109@student.hcmute.edu.vn>
2026-01-06 23:35:20 +0700

    clean

---
+++ clean_task.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM Delete scheduled tasks
+for %%T in ("DiskRamMonitor_6AM" "DiskRamMonitor_12PM" "DiskRamMonitor_6PM" "DiskRamMonitor_12AM") do (
+    schtasks /query /tn %%T >nul 2>&1
+    if %errorLevel% equ 0 (
+        schtasks /delete /tn %%T /f >nul 2>&1
+    )
+)
+
+REM Delete directories
+for %%D in ("C:\disk_ram_v21" "C:\disk_ram_v2") do (
+    if exist %%D (
+        rmdir /s /q %%D
+    )
+)
+
+exit /b 0
